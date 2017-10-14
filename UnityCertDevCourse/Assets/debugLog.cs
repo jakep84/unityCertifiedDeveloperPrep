2d4847ef405bf1442cf94acfd784c520633ac2ae
Jake <priddyjacob84@gmail.com>
2017-10-13 20:30:15 -0400

    completed layers & tags section

---
+++ UnityCertDevCourse/Assets/debugLog.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debugLog : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello World");
+        Debug.Log("Hello World 2"); 
+
+        Debug.LogWarning("Warning Message");
+        Debug.LogError("Error Message");
+	}
+}
